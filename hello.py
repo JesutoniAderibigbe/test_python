ccef988736c6a79c717ab8a3c6d328a528282da3
Jesutoni Aderibigbe <aderibigbejesutoni860@gmail.com>
2026-01-07 15:36:52 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+# This is a simple Python program that prints "hello world" in various ways
+print("hello world")
+print("hello world\n hello world ")
+print("hello" + " world")
+# input("What is your name?...")
+print("hello " + input("What is your name?..."))
---
+++ variables.py
@@ -0,0 +1,14 @@
+# This program asks for the user's name and greets them.
+name = "Jack"
+print("hello " + name)
+
+
+name = "Angela"
+print("hello " + name)
+
+print(name.__len__())  # prints the length of the name
+
+#or
+
+name = "Angela"
+print(len(name))  # prints the length of the name
